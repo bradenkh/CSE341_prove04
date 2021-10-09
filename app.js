e31c2f6ad7fb1b16b5d3f6970dba1f7aad370cea
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2021-10-08 18:37:04 -0600

    prettier changes

--- app.js
+++ app.js
@@ -35,14 +35,14 @@ app.use(errorController.get404);
 mongoose
    .connect(
       "mongodb+srv://admin:adminpass1@cluster0.bz7dt.mongodb.net/myFirstDatabase?retryWrites=true",
-      { useNewUrlParser: true }
+      { useNewUrlParser: true, useUnifiedTopology: true }
    )
    .then((result) => {
       User.findOne().then((user) => {
          if (!user) {
             const user = new User({
-               name: "Max",
-               email: "max@test.com",
+               name: "Braden",
+               email: "braden@test.com",
                cart: {
                   items: [],
                },
