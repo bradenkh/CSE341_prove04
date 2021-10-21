c21282b3f2bfa63f0bcdf957d60ae4f584e19820
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2021-10-21 10:45:19 -0600

    added shop route

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use("/shop", shopRoutes);
 app.use(authRoutes);
 
 app.get("/500", errorController.get500);
