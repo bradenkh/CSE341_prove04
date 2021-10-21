2a93865d0104d00b1c412153ba848713726fddf7
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2021-10-21 09:53:35 -0600

    adde max's code from email auth

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const path = require("path");
-const cors = require("cors");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const errorController = require("./controllers/error");
-const User = require("./models/user");
-
-//used for prove 05
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
-const session = require('express-session');
-const MongoDBStore = require("connect-mongodb-session")(session);
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,21 +17,14 @@ const MONGODB_URI =
    "mongodb+srv://admin:adminpass1@cluster0.bz7dt.mongodb.net/myFirstDatabase?retryWrites=true";
 
 const app = express();
-
 const store = new MongoDBStore({
    uri: MONGODB_URI,
-   collection: 'sessions'
- });
- const csrfProtection = csrf();
+   collection: "sessions",
+});
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-const corsOptions = {
-   origin: "https://<whispering-springs-18429>.herokuapp.com/",
-   optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -52,34 +44,49 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+   res.locals.isAuthenticated = req.session.isLoggedIn;
+   res.locals.csrfToken = req.csrfToken();
+   next();
+});
+
+app.use((req, res, next) => {
    if (!req.session.user) {
       return next();
    }
    User.findById(req.session.user._id)
       .then((user) => {
+         if (!user) {
+            return next();
+         }
          req.user = user;
          next();
       })
-      .catch((err) => console.log(err));
-});
-
-app.use((req, res, next) => {
-   res.locals.isAuthenticated = req.session.isLoggedIn;
-   res.locals.csrfToken = req.csrfToken();
-   next();
+      .catch((err) => {
+         next(new Error(err));
+      });
 });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get("/500", errorController.get500);
+
 app.use(errorController.get404);
 
+app.use((error, req, res, next) => {
+   res.status(500).render("500", {
+      pageTitle: "Error!",
+      path: "/500",
+      isAuthenticated: req.session.isLoggedIn,
+   });
+});
+
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    app.listen(PORT);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then((result) => {
+      app.listen(PORT);
+   })
+   .catch((err) => {
+      console.log(err);
+   });
